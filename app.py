1c610b8144e6993c1439c859e7c9e0ea8d73087c
Minedy <fatum@tut.by>
2019-12-17 00:19:10 +0300

    first commit here

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hey, we have Flask in a Docker container!'
+
+@app.route('/hello/<name>') 
+def hello_name(name): 
+   return 'Hello %s!' % name 
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
